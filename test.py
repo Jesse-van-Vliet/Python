96c9ac2d0ef090514fe602abbe73afffb2362709
Jesse <9021012@student.zadkine.nl>
2023-03-14 09:40:34 +0100

    PUSH

---
+++ test.py
@@ -0,0 +1,151 @@
+numbers = (1, 2, 3  )
+
+
+# Range
+# numbers = range(5, 10   , 2)
+# print(numbers)
+
+# for number in numbers: 
+#     print(number)
+
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print(item)
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i + 1
+
+
+# lists
+# names  = ["Jesse", "Piet", "Bob", "Rosanne", "Yaison"]
+# names[0] = "kaas"
+# print(names[0:3])
+# print(names)
+
+
+
+# while loops
+
+# i = 1
+# while  i <= 10:
+#     print(i * "x")
+#     i = i + 1
+
+
+# weight = float(input("weight:170  "))
+# value = input("(K)g or (L)bs ")
+# lbs = weight * 0.45
+# kg = weight / 0.45
+# 170176
+# if value == "k" or value == "K":
+#     print("Weight in lbs: " +  str(lbs))
+
+# elif value == "l" or value == "L":
+#     print("Weight in kg: " +  str(kg))
+
+# else:
+#     print("pls fill in a valid value K for Kg or L for Lbs")
+
+
+
+# temperature = 25
+
+# if temperature > 30:
+#     print("it's a hot day")
+#     print("Drink water")
+
+# elif temperature > 20:
+#     print("it's not as hot of a day")
+
+# elif temperature > 10:
+#     print("its pretty cold")
+
+# else:
+#     print("its cold")
+
+# print("done")
+
+
+# price = 25
+# print(not price > 10)
+
+# booleans
+# and (both)
+# or (at least one)
+# not (inverse values)
+
+
+# x = 3 < 2
+# print(x)
+
+
+# x = (10 + 3) * 2 
+# print(x)
+
+#operators
+# != not equal to
+# == is equal to
+# >= greater then or equal to 
+# <= less then or equal to
+# > greater then
+# < less then
+# - minus
+# + adding up
+# * multiplying  (10 * 3 = 30)  (10 ** 3 = 1000)
+# / divison (10 / 3 = 3.3333333333333335  ) (10 // 3 = 3  )  (10 % 3 = 1)
+
+# course = "python for beginners"
+
+# checks if pythn is in the string and returns True if it is in the string and False if its not in there
+# print("python" in course)
+
+# Finds the word "for" and replaces it for 4
+# if for isnt in the course string nothing will happen
+# print(course.replace("for", "4"))
+
+# Returns the number of the character if true, -1 if false
+# print(course.find("y"))
+
+
+# print(course.upper())
+# print(course)
+
+
+
+# first = float(input("enter 10 "))
+# second = float(input("enter 20 "))
+# sum = first + second
+# print("sum = " + str(sum))
+
+
+# birth_year = input("Enter your birth year: ")
+# age = 2023 - int(birth_year)
+# print(age)
+
+# int covers string "11" to a number 11
+# float() coneverts to number with float ( 11.1, 22.2 3.33)
+# bool() converts string to boolean
+# str()  converts to string
+
+
+
+
+# # answer from the input gets stored in the variable name
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+
+# #numbers
+# 10
+# 11
+# 12
+# 13
+
+# #strings 
+# "Jesse"
+# "worden"
+# "eerste keer python"
+
+# #boolean's
